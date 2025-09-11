a7f3d4341d72e467561dcbe3c20f3fcf71b21b07
kenneth.brattli <kenneth.brattli@telenor.no>
2025-09-11 16:48:31 +0200

    example 1

--- app/examples/example1.tsx
+++ app/examples/example1.tsx
@@ -1,40 +1,41 @@
-import { Stack } from "expo-router";
 import { StyleSheet, View } from "react-native";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { Pressable } from "react-native-gesture-handler";
 import Animated, {
     useAnimatedStyle,
     useSharedValue,
     withTiming,
 } from "react-native-reanimated";
 
-export default function Example() {
+function Example() {
     const scale = useSharedValue(1);
 
     const animatedStyle = useAnimatedStyle(() => ({
         transform: [{ scale: scale.value }],
     }));
 
-    const tap = Gesture.Tap()
-        .onBegin(() => {
-            "worklet";
-            scale.value = withTiming(2);
-        })
-        .onFinalize(() => {
-            "worklet";
-            scale.value = withTiming(1);
-        });
+    const onPressIn = () => {
+        scale.value = withTiming(2);
+    };
+
+    const onPressOut = () => {
+        scale.value = withTiming(1);
+    };
 
     return (
-        <>
-            <Stack.Screen options={{ title: "Basic example" }} />
-            <View style={styles.container}>
-                <GestureDetector gesture={tap}>
-                    <Animated.View style={[styles.button, animatedStyle]} />
-                </GestureDetector>
-            </View>
-        </>
+        <Pressable onPressIn={onPressIn} onPressOut={onPressOut}>
+            <Animated.View style={[styles.button, animatedStyle]} />
+        </Pressable>
     );
 }
+
+export default function Screen1() {
+    return (
+        <View style={styles.container}>
+            <Example />
+        </View>
+    );
+}
+
 const styles = StyleSheet.create({
     button: {
         width: 100,
